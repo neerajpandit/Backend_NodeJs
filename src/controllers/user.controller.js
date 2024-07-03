2f718603491052ae6cec71547c019ce04613d3cc
neerajpandit <pandeyneeraj1507@gmail.com>
2024-07-03 15:49:36 +0530

    inikk

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -101,7 +101,7 @@ const loginUser = asyncHandler(async (req, res) => {
         httpOnly: true,
         secure: true
     }
-
+    console.log("Setting cookies:", { accessToken, refreshToken });
     return res
         .status(200)
         .cookie("accessToken", accessToken, options)
