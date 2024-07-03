9c3e4abd288320cc5b9fd1a06fa66e2df793430a
neerajpandit <pandeyneeraj1507@gmail.com>
2024-07-03 13:35:33 +0530

    add session

--- src/app.js
+++ src/app.js
@@ -18,13 +18,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
-// app.use(
-//   session({
-//     secret: process.env.ACCESS_TOKEN_SECRET, // Replace with your secret key
-//     resave: false,
-//     saveUninitialized: false,
-//   }),
-// );
+app.use(
+  session({
+    secret: process.env.ACCESS_TOKEN_SECRET, // Replace with your secret key
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 // app.use(passport.initialize());
 // app.use(passport.session());
 
